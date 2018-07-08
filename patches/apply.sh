3861ca8927fe3e89fad2ff57b6539c020d7cfe32
Samad SegMane <32767790+MrSaMaDo@users.noreply.github.com>
2018-07-08 04:30:19 -0700

    General: Patches For All MTK

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
  cd ../../../..
-
  cd packages/apps/Settings
- patch -p1 -b < ../../../device/Micromax/A102/patches/android_settings_developer_info.patch
+ patch -p1 -b < ../../../device/*/A102/patches/android_settings_developer_info.patch
  git clean -f -d
 
  cd ../../../system/vold
- patch -p1 < ../../device/Micromax/A102/patches/android_system_vold.patch
+ patch -p1 < ../../device/*/*/patches/android_system_vold.patch
  
  cd ../sepolicy
- patch -p1 < ../../device/Micromax/A102/patches/android_system_sepolicy.patch
+ patch -p1 < ../../device/*/*/patches/android_system_sepolicy.patch
+ 
  cd ../../external/icu
- patch -p1 < ../../device/Micromax/A102/patches/android_external_icu.patch
+ patch -p1 < ../../device/*/*/patches/android_external_icu.patch
 
  cd ../../frameworks/av
- patch -p1 < ../../device/Micromax/A102/patches/android_frameworks_av.patch
+ #patch -p1 < ../../device/*/*/patches/android_frameworks_av.patch
 
- cd ..
- cd base
- #patch -p1 < ../../device/Micromax/A102/patches/android_frameworks_base.patch
+ cd ../base
+ #patch -p1 < ../../device/*/*/patches/android_frameworks_base.patch
 
  cd ../..
  cd packages/apps/FMRadio
- patch -p1 < ../../../device/Micromax/A102/patches/android_packages_apps_FMRadio.patch
+ patch -p1 < ../../../device/*/*/patches/android_packages_apps_FMRadio.patch
 
  cd ../../../
 
  echo Successfuly patched!
+ echo Now: Pick MT6572 Changes.
