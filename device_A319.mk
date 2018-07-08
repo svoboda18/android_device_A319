8712089704e79046fb87ad900c7fd0c3ef89754a
Samad SegMane <32767790+MrSaMaDo@users.noreply.github.com>
2018-07-08 14:31:33 -0700

    A319:Stuck in logo FIX

--- device_A319.mk
+++ device_A319.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+$(call inherit-product-if-exists, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/Micromax/A102/A102-vendor.mk)
+$(call inherit-product, vendor/Lenovo/A319/A319-vendor.mk)
 
 LOCAL_PATH := device/Lenovo/A319
 
