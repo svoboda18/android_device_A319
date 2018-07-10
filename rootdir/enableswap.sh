12ddbb399c72a5d01c094e68ca0feee9ec1bc013
Samad SegMane <32767790+MrSaMaDo@users.noreply.github.com>
2018-07-10 18:25:36 +0100

    Boot.img: Fix Swap (Zram0).

--- rootdir/enableswap.sh
+++ rootdir/enableswap.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 echo $((384*1024*1024)) > /sys/block/zram0/disksize
+chmod 777 /system/bin/tiny_mkswap
+chmod 777 /system/bin/tiny_swapon
+chmod 777 /system/bin/tiny_swapoff
 /system/bin/tiny_mkswap /dev/block/zram0
 /system/bin/tiny_swapon /dev/block/zram0
