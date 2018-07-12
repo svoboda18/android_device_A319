23efc35cc862ef66f79f862a824d814493d25f0c
Samad SegMane <32767790+MrSaMaDo@users.noreply.github.com>
2018-07-12 11:59:40 +0100

    Boot: Try Fix Bootloop #1.

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -1,24 +1,23 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-             AudioStubs.cpp \
-             Bionic.cpp \
-             EGL.cpp \
-             GraphicBufferMapper.cpp \
-             Icu53.cpp \
-             Icu55.cpp \
-             MediaBufferGroup.cpp \
-             MediaSource.cpp \
-             Player.cpp \
-             RIL.cpp \
-             SSL.cpp \
-             WVM.cpp \
-             Xlog.cpp
+  AudioStubs.cpp \
+  Bionic.cpp \
+  EGL.cpp \
+  GraphicBufferMapper.cpp \
+  Icu53.cpp \
+  Icu55.cpp \
+  MediaBufferGroup.cpp \
+  MediaSource.cpp \
+  Player.cpp \
+  RIL.cpp \
+  SSL.cpp \
+  WVM.cpp \
+  Xlog.cpp
 
 LOCAL_SHARED_LIBRARIES := libbinder libcutils libutils liblog libicuuc libui libcrypto libc libssl
 LOCAL_MODULE := libmtk_symbols
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
