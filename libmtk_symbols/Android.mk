f83d1ae64a48085326cbbe74d7e78a422e135a54
MrSaMaDo <MrSaMado@Github.com>
2018-07-12 16:38:07 +0000

    Revert "Update Android.mk"
    This reverts commit 930ca1f7be4707d0f1023027df80aa63d46b7698.

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -2,20 +2,19 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-  SSL.cpp \
-  WVM.cpp \
-  Xlog.cpp \
   AudioStubs.cpp \
   Bionic.cpp \
   EGL.cpp \
   GraphicBufferMapper.cpp \
+  Icu53.cpp \
+  Icu55.cpp \
   MediaBufferGroup.cpp \
   MediaSource.cpp \
   Player.cpp \
   RIL.cpp \
-  Icu51.cpp \
-  Icu53.cpp \
-  Icu55.cpp
+  SSL.cpp \
+  WVM.cpp \
+  Xlog.cpp
 
 LOCAL_SHARED_LIBRARIES := libbinder libcutils libutils liblog libicuuc libui libcrypto libc libssl
 LOCAL_MODULE := libmtk_symbols
