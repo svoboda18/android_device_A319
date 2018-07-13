fa7a80c56461aaaf6b72be9c4fa45936501e477e
Samad SegMane <32767790+MrSaMaDo@users.noreply.github.com>
2018-07-13 04:37:48 -0700

    Media: Try Fix. #3

--- libmtk_symbols/MediaBufferGroup.cpp
+++ libmtk_symbols/MediaBufferGroup.cpp
@@ -1,5 +1,5 @@
 extern "C" {
 void _ZN7android16MediaBufferGroupC1Ev() { }
-
+void _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE() { }
 void _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferE() { }
 }
