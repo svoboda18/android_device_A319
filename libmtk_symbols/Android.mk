ac4f313727e587207a80bcfa58ce111f76a81be4
MrSaMaDo <MrSaMado@Github.com>
2018-07-13 19:04:47 +0000

    Reverts

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -16,7 +16,6 @@ LOCAL_SRC_FILES := \
              WVM.cpp \
              Xlog.cpp
              
-LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
 LOCAL_SHARED_LIBRARIES := libbinder libcutils libutils liblog libicuuc libui libcrypto libc libssl
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
