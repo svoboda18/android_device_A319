5b4d2eab80214104f656da52d9fee1a53afc4baa
MrSaMaDo <MrSaMado@Github.com>
2018-07-13 19:03:30 +0000

    Reverts

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -11,14 +11,13 @@ LOCAL_SRC_FILES := \
              MediaBufferGroup.cpp \
              MediaSource.cpp \
              Player.cpp \
-             mtk_omx.cpp \
              RIL.cpp \
              SSL.cpp \
              WVM.cpp \
              Xlog.cpp
              
 LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
-LOCAL_SHARED_LIBRARIES := libbinder libcutils libutils liblog libicuuc libui libcrypto libc libssl libstagefright_foundation
+LOCAL_SHARED_LIBRARIES := libbinder libcutils libutils liblog libicuuc libui libcrypto libc libssl
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
 
