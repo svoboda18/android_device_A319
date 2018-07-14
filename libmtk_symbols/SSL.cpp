ed9b7ca1b34c1be25075b1c22fe8056dd931896c
Samad SegMane <32767790+MrSaMaDo@users.noreply.github.com>
2018-07-14 12:16:30 +0100

    MT6572: Correct GPS and WVM.

--- libmtk_symbols/SSL.cpp
+++ libmtk_symbols/SSL.cpp
@@ -1,4 +1,5 @@
 #include <openssl/ssl.h>
+#include <malloc.h>
 
 #define LOG_TAG "SSL_ctrl_stub"
 #include <log/log.h>
@@ -25,3 +26,15 @@ void *CRYPTO_malloc(int num, const char *file, int line) {
 void CRYPTO_lock(int mode, int type, const char *file, int line) {
     ALOGD("CRYPTO_lock: mode=%d type=%d file=%s line=%d", mode, type, file, line);
 }
+
+void SSLv2_client_method() { }
+
+void SSL_CTX_callback_ctrl() { }
+
+void SSL_CTX_ctrl() { }
+
+void UI_OpenSSL() { }
+
+void SSL_ctrl() { }
+
+void OPENSSL_add_all_algorithms_noconf() { }
