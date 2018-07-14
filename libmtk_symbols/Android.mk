cfdb152df3ede2ee1ea40430c6e764cf4d011fb5
Samad SegMane <32767790+MrSaMaDo@users.noreply.github.com>
2018-07-14 09:50:25 -0700

    Update Android.mk

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -2,21 +2,22 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
+             SSL.c \
              AudioStubs.cpp \
              Bionic.cpp \
              EGL.cpp \
              GraphicBufferMapper.cpp \
-             Icu55.c \
              MediaBufferGroup.cpp \
              MediaSource.cpp \
              Player.cpp \
              RIL.cpp \
-             SSL.c \
              WVM.cpp \
-             Xlog.cpp
+             Xlog.cpp \ 
+             Icu51.c \
+             Icu53.c \
+             Icu55.c
 
-LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
-LOCAL_SHARED_LIBRARIES := libcutils liblog libutils libbinder             
+LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/        
 LOCAL_SHARED_LIBRARIES := libbinder libcutils libutils liblog libicuuc libui libcrypto libc libssl
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
