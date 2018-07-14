b98cc755a7a44aa376141397ac0bc2fe43f7e247
MrSaMaDo <MrSaMado@Github.com>
2018-07-14 09:16:14 +0000

    MT6572: Improve AUDIO & Fix Build

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -15,7 +15,9 @@ LOCAL_SRC_FILES := \
              SSL.cpp \
              WVM.cpp \
              Xlog.cpp
-             
+
+LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
+LOCAL_SHARED_LIBRARIES := libcutils liblog libutils libbinder             
 LOCAL_SHARED_LIBRARIES := libbinder libcutils libutils liblog libicuuc libui libcrypto libc libssl
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
