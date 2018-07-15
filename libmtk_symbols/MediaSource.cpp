3deabf316fd9a6302a2c0e9267bb89a0e5b8f67a
MrSaMaDo <MrSaMado@Github.com>
2018-07-15 16:17:35 +0000

    Trying..

--- libmtk_symbols/MediaSource.cpp
+++ libmtk_symbols/MediaSource.cpp
@@ -1,1 +1,4 @@
-extern "C" void _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE() { }
+extern "C"{ 
+            void _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE() { }
+            void _ZNK7android11MediaSource11ReadOptions14getNonBlockingEv() { }
+}
--- libmtk_symbols/WVM.cpp
+++ libmtk_symbols/WVM.cpp
@@ -5,5 +5,4 @@ extern "C" {
 
 	void _ZN7android16CtaTrustedClient26IsCtaTrustedGetTokenClientERKNS_7String8E() { }
  
-        void _Z24openssl_generate_keypairPK16keymaster_device19keymaster_keypair_tPKvPPhPj() { }
 }
