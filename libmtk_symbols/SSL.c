09918431b138fb534bae5627e91ba88c5e70d9b4
MrSaMaDo <MrSaMado@Github.com>
2018-07-15 03:54:58 +0000

    Fix Build

--- libmtk_symbols/SSL.c
+++ libmtk_symbols/SSL.c
@@ -3,16 +3,6 @@
 #define LOG_TAG "SSL_ctrl_stub"
 #include <log/log.h>
 
-long SSL_ctrl(SSL *ssl, int cmd, long larg, void *parg) {
-    ALOGD("SSL_ctrl: ssl=%p cmd=%d larg=%ld parg=%p", ssl, cmd, larg, parg);
-    return -1;
-}
-
-long SSL_CTX_ctrl(SSL_CTX *ctx, int cmd, long larg, void *parg) {
-    ALOGD("SSL_CTX_ctrl: ctx=%p cmd=%d larg=%ld parg=%p", ctx, cmd, larg, parg);
-    return -1;
-}
-
 void CRYPTO_free(void *str) {
     free(str);
 }
