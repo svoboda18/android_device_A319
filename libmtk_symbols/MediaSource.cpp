97b61932c25661e214c04f31518d3b0357e4557c
Samad SegMane <32767790+MrSaMaDo@users.noreply.github.com>
2018-07-16 04:06:11 -0700

    Update MediaSource.cpp

--- libmtk_symbols/MediaSource.cpp
+++ libmtk_symbols/MediaSource.cpp
@@ -1,3 +1,1 @@
-extern "C"{ 
-void _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE() { }
-}
+extern "C" void _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE() { }
